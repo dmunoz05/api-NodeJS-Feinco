e80f2fdcf2eb33012874a14afbbcf2638d9e1124
= <daniel20025febrero29@gmail.com>
2023-09-23 17:28:01 -0500

    New endpoint types credits

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ dotenv.config();
 
 /* Database */
 const config = {
-    host: "185.239.210.1",
-    port: "3306",
-    user: "u224736136_RfpIO",
-    password: "|bn/+ilB@8",
-    database: "u224736136_yHrbs",
-  };
-  
-  const pool = mysql2.createPool(config);
+  host: "185.239.210.1",
+  port: "3306",
+  user: "u224736136_RfpIO",
+  password: "|bn/+ilB@8",
+  database: "u224736136_yHrbs",
+};
+
+const pool = mysql2.createPool(config);
 
 
 const PORT = 3000;
@@ -26,11 +26,11 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 const corsOptions = {
-    // origin: "https://plataformain.com", 
-    origin: "*",
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    credentials: true, // Habilita el intercambio de cookies y encabezados de autorización
-    optionsSuccessStatus: 204,
+  // origin: "https://plataformain.com", 
+  origin: "*",
+  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+  credentials: true, // Habilita el intercambio de cookies y encabezados de autorización
+  optionsSuccessStatus: 204,
 };
 
 //Habiliar los cors
@@ -38,18 +38,30 @@ app.use(cors(corsOptions));
 
 //Escucha
 app.listen(PORT, () => {
-    console.log('Running server on port', PORT)
+  console.log('Running server on port', PORT)
 });
 
 
 // Rutas del app
 app.get('/wpxfeinco/', async (req, res) => {
-    res.json({message: 'Prueba backend Feinco'})
+  res.json({ message: 'Prueba backend Feinco' })
 });
 
 // Rutas del app
 app.get('/wpxfeinco/users_report/', async (req, res) => {
-    const sql = 'SELECT * FROM u224736136_yHrbs.wp_users_excel_credit;';
+  const sql = 'SELECT * FROM u224736136_yHrbs.wp_users_excel_credit;';
+  pool.query(sql, (err, result) => {
+    if (err) {
+      return res.status(500).json({ message: "Error en la consulta", error: err });
+    }
+    return res.json(result);
+  });
+});
+
+
+// Rutas del app
+app.get('/wpxfeinco/types_credit/', async (req, res) => {
+  const sql = 'SELECT * FROM u224736136_yHrbs.wp_type_credit_dates;';
   pool.query(sql, (err, result) => {
     if (err) {
       return res.status(500).json({ message: "Error en la consulta", error: err });
