0f519e84206cc18c5b6e6e87484af563793fc110
= <daniel20025febrero29@gmail.com>
2023-09-23 17:17:28 -0500

    Config endpoint users report

--- index.js
+++ index.js
@@ -4,6 +4,18 @@ const express = require('express');
 const mysql2 = require('mysql2');
 dotenv.config();
 
+/* Database */
+const config = {
+    host: "185.239.210.1",
+    port: "3306",
+    user: "u224736136_RfpIO",
+    password: "|bn/+ilB@8",
+    database: "u224736136_yHrbs",
+  };
+  
+  const pool = mysql2.createPool(config);
+
+
 const PORT = 3000;
 
 // SERVIDOR
@@ -33,4 +45,15 @@ app.listen(PORT, () => {
 // Rutas del app
 app.get('/wpxfeinco/', async (req, res) => {
     res.json({message: 'Prueba backend Feinco'})
+});
+
+// Rutas del app
+app.get('/wpxfeinco/users_report/', async (req, res) => {
+    const sql = 'SELECT * FROM u224736136_yHrbs.wp_users_excel_credit;';
+  pool.query(sql, (err, result) => {
+    if (err) {
+      return res.status(500).json({ message: "Error en la consulta", error: err });
+    }
+    return res.json(result);
+  });
 });
