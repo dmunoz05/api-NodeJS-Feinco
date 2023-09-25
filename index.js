1fddd6d6043f6bb8ef41d7025f34c3bd62905b5b
= <daniel20025febrero29@gmail.com>
2023-09-24 19:22:03 -0500

    Crypto JS

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 const express = require('express');
 const mysql2 = require('mysql2');
+const CryptoJS = require("crypto-js");
 dotenv.config();
 
 /* Database */
